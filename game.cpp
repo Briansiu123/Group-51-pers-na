014e289d91584ce0992e90fc1667a3a9a4c12918
Briansiu123 <63183463+Briansiu123@users.noreply.github.com>
2020-04-28 18:37:35 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,50 +1,68 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
-int merchant(int &souls,int &hp,int &atk, int &lv){
-  cout<<"Welcome to the Witcher Shop."<<endl;
+int merchant(int &souls, int &hp, int &atk, int &lv, int &mp, int &combustion){
+  cout<<"Welcome to the Withcer Shop."<<endl;
   cout<<"What do you want to improve?"<<endl;
-  cout<<"your hp:"<<hp<<" "<<"your atk:"<<atk<<endl;
+  cout<<"Your level:"<<lv<<" "<<"Your hp:"<<hp<<" "<<"Your atk:"<<atk<<endl;
   char cont;
   cont='Y';
   while(cont=='Y'){
-    cout<<"The amount of souls you have:$"<< souls << endl;
-    cout<<"armor:$2, spells:$4"<<endl;
-    cout<<"please enter what you want to improve:";
+    cout<<"The amount of souls you have:$"<<souls<<endl;
+    cout<<"armor:$2, spells:$4, combustion(a power):$20"<<endl;
+    cout<<"Please enter what you want to improve/learn:";
     string item;
-    cin >> item;
+    cin>>item;
     cout<<endl;
     if(item=="armor"){
       if(souls>=2){
         hp+=3;
         souls-=2;
         lv++;
-        cout<<"your hp has increased"<<endl;
+        mp++;
+        cout<<"Your hp has increased"<<endl;
       }
       else
-      cout<<"Sorry, you don't have enough souls."<<endl;
+      cout<<"Sorry, you don't have enough souls"<<endl;
     }
     else if(item=="spells"){
       if(souls>=4){
         atk+=2;
         souls-=4;
         lv++;
-        cout<<"your atk has increased"<<endl;
+        mp++;
+        cout<<"Your atk has increased."<<endl;
       }
       else
       cout<<"Sorry, you don't have enough souls."<<endl;
     }
-    else{
-      cout<<"This ability doesn't exist."<<endl;
+    else if(item=="combustion"){
+      if(combustion==0){
+        if(souls>=20){
+          combustion=1;
+          souls-=20;
+          lv++;
+          mp++;
+          cout<<"You can now use combustion in battle."<<endl;
+        }
+        else
+        cout<<"Sorry, you don't have enough souls."<<endl;
+      }
+      else
+      cout<<"You have already learnt combustion."<<endl;
     }
-    cout<<"your hp:"<<hp<<" "<<"your atk:"<<atk<<endl;
-    cout<<"Is there anything you still want to improve? (Y/N):";
+    else
+    cout<<"This ability doesn't exist."<<endl;
+    cout<<"Your level:"<<lv<<" "<<"Your hp:"<<hp<<" "<<"Your atk:"<<atk<<endl;
+    cout<<"Is there anything you still want to improve? (Y/N)";
     cin>>cont;
+    cout<<endl;
   }
   cout<<"Have a nice day."<<endl;
   return 0;
 }
 
-int battle(int &souls, int &hp, int &atk, int &lv, int &flasks){
+int battle(int &souls, int hp, int atk, int lv, int flasks, int mp, int combustion){
   int dungeon,tem;
   char explore='Y';
   while(explore=='Y'){
@@ -54,7 +72,7 @@ int battle(int &souls, int &hp, int &atk, int &lv, int &flasks){
     cout<<endl;
     while(tem>0){
       srand(dungeon);
-      
+
     }
 
     cout<<"Continue on exploring? (Y/N)";
@@ -65,12 +83,15 @@ int battle(int &souls, int &hp, int &atk, int &lv, int &flasks){
 }
 
 int main(){
-  int souls, hp, atk,lv,flasks;
+  int souls,hp,atk,lv,flasks,mp,fireball,combustion;
   hp=5;
   atk=1;
   souls=10;
   lv=1;
   flasks=3;
-  merchant(souls,hp,atk,lv);
+  mp=2;
+  combustion=0;
+  merchant(souls,hp,atk,lv,mp,combustion);
+  battle(souls, hp, atk, lv, flasks,mp,combustion);
   return 0;
 }

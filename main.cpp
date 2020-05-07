b83c6ac5707dcedcf71c176d39747445b4ce3df0
orkork123 <63357790+orkork123@users.noreply.github.com>
2020-05-07 20:50:08 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -614,9 +614,10 @@ int main()
     if (personality >7 && personality <10){
         cout<<"YOLO";
         srand(time(NULL));	
-        for (int i = 0; i < 5; ++i){ 
-            cout << rand() % 10 + 1 << endl;
-        }
+        strength =  rand() % 10 + 1;
+        intelligence =  rand() % 10 + 1;
+        agility =  rand() % 10 + 1;
+        luck =  rand() % 10 + 1;
         cout<<"Strength : "<<strength<<endl;
         cout<<"Intelligence : "<<intelligence<<endl;
         cout<<"Agility : "<<agility<<endl;
